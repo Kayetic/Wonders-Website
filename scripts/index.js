0ee001c57b903fcd3b8a79598c4d2ea16fc661ee
Kayetic <bartek.bak@protonmail.com>
2023-05-11 15:10:03 +0100

    added initial files

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const menuItemsList = document.querySelectorAll(".menu-item");
+const overlay = document.getElementById("overlay");
+
+let hidden = true;
+
+hamburgerMenu.addEventListener("click", function () {
+  console.log("clicked");
+  if (hidden) {
+    menuItemsList.forEach((item, index) => {
+      item.style.transform = "translateX(0)";
+    });
+    overlay.style.opacity = "100%";
+    hamburgerMenu.style.opacity = "100";
+    hidden = false;
+  } else {
+    menuItemsList.forEach((item, index) => {
+      item.style.transform = "translateX(-400%)";
+    });
+    hamburgerMenu.style.opacity = "65%";
+    overlay.style.opacity = "0%";
+    hidden = true;
+  }
+});
