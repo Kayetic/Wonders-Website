4f3bf9e3ede5f85016e7d5c34ed4ae170e7584e1
Kayetic <bartek.bak@protonmail.com>
2023-05-18 09:56:16 +0100

    started working on navbar style info; moved all files with floating elements to separate folder

---
+++ archive/index-with-animated.js
@@ -0,0 +1,80 @@
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const menuItemsList = document.querySelectorAll(".menu-item");
+const overlay = document.getElementById("overlay");
+
+const leftElement = document.getElementById("left-element");
+const rightElement = document.getElementById("right-element");
+
+let hidden = true;
+
+hamburgerMenu.addEventListener("click", function () {
+  console.log("clicked");
+  if (hidden) {
+    menuItemsList.forEach((item, index) => {
+      item.style.transform = "translateX(0)";
+    });
+    overlay.style.opacity = "100%";
+    hamburgerMenu.style.opacity = "100";
+    hidden = false;
+  } else {
+    menuItemsList.forEach((item, index) => {
+      item.style.transform = "translateX(-400%)";
+    });
+    hamburgerMenu.style.opacity = "65%";
+    overlay.style.opacity = "0%";
+    hidden = true;
+  }
+});
+
+// detecting ctrl key
+function onCtrlKeyDown(event) {
+  if (event.ctrlKey) {
+    // The `Ctrl` key is down.
+    console.log("ctrl key down");
+  }
+}
+document.addEventListener("keydown", onCtrlKeyDown);
+
+const centeredElement = document.querySelector(".centered-element");
+const boundingElement = document.querySelector(".bounding-element");
+
+const firstFloating = document.querySelector(".first-floating");
+const secondFloating = document.querySelector(".second-floating");
+const thirdFloating = document.querySelector(".third-floating");
+const fourthFloating = document.querySelector(".fourth-floating");
+
+let hiddenFloating = [];
+
+const movementFunction = function (elementName, speedToUse) {
+  const elementToChange = document.querySelector(elementName);
+
+  let mouseX = 0;
+  let mouseY = 0;
+
+  let ballX = 0;
+  let ballY = 0;
+
+  let speed = speedToUse;
+
+  function animate() {
+    let distX = mouseX - ballX;
+    let distY = mouseY - ballY;
+
+    ballX = ballX + distX * speed;
+    ballY = ballY + distY * speed;
+
+    elementToChange.style.left = ballX + "px";
+    elementToChange.style.top = ballY + "px";
+
+    requestAnimationFrame(animate);
+  }
+  animate();
+
+  document.addEventListener("mousemove", function (event) {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+  });
+};
+
+movementFunction("div.first-floating", 0.04);
+movementFunction("div.second-floating", 0.01);
