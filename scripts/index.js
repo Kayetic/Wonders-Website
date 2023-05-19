9549bd86746c29eef320b0ff0d85bfcab0461398
Kayetic <bartek.bak@protonmail.com>
2023-05-19 14:28:27 +0100

    🎨 style(index.html, index.css): update navigation items to have classes and adjust styles 🚀 feat(index.js): add click event listener to first navigation item to expand and add text

--- scripts/index.js
+++ scripts/index.js
@@ -34,3 +34,19 @@ function onCtrlKeyDown(event) {
   }
 }
 document.addEventListener("keydown", onCtrlKeyDown);
+
+const firstNavItem = document.querySelector(".nav--1");
+const secondNavItem = document.querySelector(".nav--2");
+const thirdNavItem = document.querySelector(".nav--3");
+const fourthNavItem = document.querySelector(".nav--4");
+
+firstNavItem.addEventListener("click", function () {
+  firstNavItem.style.backgroundColor = "#f2f2f2";
+  firstNavItem.style.color = "#000";
+  // set height to 80vh
+  firstNavItem.style.height = "fit-content";
+  // add lorem ipsum text under the heading of the nav item
+  firstNavItem.innerHTML += `<p class="nav--1--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>`;
+  // set width to double the current width
+  firstNavItem.style.width = "20vw";
+});
